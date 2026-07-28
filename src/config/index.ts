dc99dca0a7d36a6b7958f67e664b26a6c662332e
Punit Sharma <ssmudgal01@gmail.com>
2026-07-28 21:38:04 +0530

    refactor server file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,11 @@
 import { config } from "dotenv";
 import { resolve } from "path";
 
+const env = process.env.NODE_ENV || "development";
+
 config({
-    path: resolve(__dirname, "../../.env"),
+    path: resolve(__dirname, `../../.env.${env}`),
+    override: true,
 });
 
 const { PORT, DATABASE_URL } = process.env;
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import { Hono } from 'hono'
 import authRoutes from './routes/auth.route'
+import { logger } from './utils/logger' 
+import {logger as honoLogger} from 'hono/logger'
 
-const app = new Hono()
+const app = new Hono().basePath('/api/v1')
 
-
-
+app.use("*", honoLogger())
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.route('api/v1/auth', authRoutes);
-
-
+app.route('/auth', authRoutes)
 
 export default app
