3f65bd63d4be2ddd7fcfc474e88019376477bd7b
Punit Sharma <ssmudgal01@gmail.com>
2026-07-28 21:59:06 +0530

    refactor

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,11 @@
 import { pino } from "pino";
 
+const showLogs = process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test";
 export const logger = pino({
   level: process.env.LOG_LEVEL ?? "info",
 
   transport:
-    process.env.NODE_ENV === "development"
+    showLogs
       ? {
           target: "pino-pretty",
           options: {
