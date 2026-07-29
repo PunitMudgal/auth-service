5ce4c92d2055e9f971b8663c551ec1db00fac1ca
Punit Sharma <ssmudgal01@gmail.com>
2026-07-29 11:12:11 +0530

    feat: hash password

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,13 +1,15 @@
 import { db } from "../db/connection";
 import { users } from "../db/schema";
 import { RegisterUser, User } from "../types";
+import * as bcrypt from "bcrypt";
 
 export class AuthService { 
 
     async register({email, password, firstName, lastName}: RegisterUser){
+        const hashedPassword = await bcrypt.hash(password, 3);
         const user = await db.insert(users).values({
             email,
-            password,   
+            password: hashedPassword,   
             firstName,
             lastName,
         }).returning({firstName: users.firstName, lastName: users.lastName, email: users.email});
